6a7ed3db0b9a7784fbc827ae10b2926a008230fc
loooddy <dkaxo123@naver.com>
2022-09-14 10:40:06 +0900

    starbuskc-respon

--- js/main.js
+++ js/main.js
@@ -125,9 +125,9 @@ function floatingObject(selector, delay, size){
   });
 }
 
-floatingObject(".floating1", 1, 15);
-floatingObject(".floating2", .5, 15);
-floatingObject(".floating3", 1.5, 20);
+// floatingObject(".floating1", 1, 15);
+// floatingObject(".floating2", .5, 15);
+// floatingObject(".floating3", 1.5, 20);
 
 // SCROLL MAGIC
 const spyEls = document.querySelectorAll("section.scroll-spy");
